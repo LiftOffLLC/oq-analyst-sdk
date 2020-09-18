07f186cbf3aec1ee7b32ed33c2a7952bd24b0c5b
sspathare97 <sspathare97@gmail.com>
2020-09-19 01:48:10 +0530

    update, reactive client; insert questionnaire

--- src/main/clients.js
+++ src/main/clients.js
@@ -1,23 +1,31 @@
 const Rest = require('./helper/rest');
+const genderMap = { 0: 'Unknown', 1: 'Male', 2: 'Female', 9: 'Unknown' };
 
 class Clients extends Rest {
   async getAll() {
     return await this.request('/GetAllClients');
   }
 
-  async insert({ firstName, lastName, dobMDY, gender, isMinor }) {
-    const genderMap = { 0: 'Unknown', 1: 'Male', 2: 'Female', 9: 'Unknown' };
+  async insert({
+    medicalRecordNumber,
+    firstName,
+    lastName,
+    dobMDY,
+    gender = 0,
+    isMinor,
+    clinicianId = 5,
+  }) {
     const Gender = genderMap[gender];
     const InstrumentCode = isMinor ? 'Y-OQ30ESR' : 'OQ30E';
     const client = {
-      MedRecordNum: '',
+      MedRecordNum: medicalRecordNumber,
       FirstName: firstName,
       MiddleName: '',
       LastName: lastName,
       BirthDate: dobMDY,
       Gender,
       Clinic: 'Shop 1',
-      ClinicianPersonID: '5',
+      ClinicianPersonID: clinicianId,
       ClinicianCustomID: '',
       InstrumentCode,
       DefaultSettingOfCare: 'Outpatient',
@@ -27,11 +35,52 @@ class Clients extends Rest {
     return await this.request('/InsertClient_081902', client);
   }
 
-  async delete(id) {
+  async update({
+    medicalRecordNumber,
+    newMedicalRecordNumber = '',
+    firstName = '',
+    lastName = '',
+    dobMDY = '',
+    gender,
+    isMinor,
+    clinicianId = '',
+  }) {
+    const strGender = gender !== undefined ? genderMap[gender] : '';
+    let strDefaultInstrumentCode = '';
+    if (isMinor !== undefined) {
+      strDefaultInstrumentCode = isMinor ? 'Y-OQ30ESR' : 'OQ30E';
+    }
     const client = {
-      ClientID: id,
+      strMedicalRecordNumber: medicalRecordNumber,
+      strNewMedicalRecordNumber: newMedicalRecordNumber,
+      strFirstName: firstName,
+      strMiddleName: '',
+      strLastName: lastName,
+      strBirthDate: dobMDY,
+      strGender,
+      strClinic: '',
+      strClinicianID: clinicianId,
+      strClinicianCustomID: '',
+      strDefaultInstrumentCode,
+      strDefaultSettingOfCare: '',
+      strDiagnosis: '',
+      strSessionNumberIncrement: '',
+    };
+    return await this.request('/UpdateClient_081902', client);
+  }
+
+  async inactivate(clientId) {
+    const client = {
+      ClientID: clientId,
     };
     return await this.request('/InactivateClient', client);
+  }
+
+  async reactivate(clientId) {
+    const client = {
+      ClientID: clientId,
+    };
+    return await this.request('/ReactivateClient', client);
   }
 }
 
--- src/main/questionnaires.js
+++ src/main/questionnaires.js
@@ -1,12 +1,12 @@
 const Rest = require('./helper/rest');
 
 class Questionnaires extends Rest {
-  async get({
+  async getAll({
     strStartDate,
     strEndDate,
-    MedRecordNumber,
-    EpisodeType,
-    InstrumentIDs,
+    MedRecordNumber = '',
+    EpisodeType = '',
+    InstrumentIDs = '',
   }) {
     return await this.request('/GetQuestionnaires_071701', {
       strStartDate,
@@ -15,6 +15,31 @@ class Questionnaires extends Rest {
       EpisodeType,
       InstrumentIDs,
     });
+  }
+
+  async insert({
+    medicalRecordNumber,
+    isMinor,
+    sessionNumber,
+    dateMDY,
+    answers,
+  }) {
+    const iInstrumentID = isMinor ? 5 : 4;
+    const strResponseData = answers.join('~');
+    const questionnaire = {
+      strPersonID: '',
+      strMedicalRecordNumber: medicalRecordNumber,
+      iInstrumentID,
+      strClinicName: 'Shop 1',
+      strCompletedByCode: 'Self',
+      strSettingOfCare: 'Outpatient',
+      strSessionNumber: sessionNumber,
+      strAdministrationDate: dateMDY,
+      strOutpatientFlag: true,
+      strResponseDataType: 'byvalue',
+      strResponseData,
+    };
+    return await this.request('/InsertQuestionnaire_081801', questionnaire);
   }
 }
 
